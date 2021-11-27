dcdf9a5907bf02d9099d39ee378b59e92bb0c3c3
richanghmc <sg626.richardchang@gmail.com>
2021-11-26 17:13:26 -0800

    dictionary

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     title = "This is the homepage"
-    text = "type /fizzbuzz/(insert number) or /words/(insert word) in the url"
-
-    return render_template('homepage.html', title=title, text=text)
+    return render_template('homepage.html', title=title)
 
 @app.route("/fizzbuzz/<int:n>")
 def fizzbuzz(n):
@@ -42,3 +40,22 @@ def fizzbuzzString(word):
         if sorted(word.upper()) == sorted(i):
             anagrams.append(i)
     return render_template('words.html', anagrams=anagrams)
+
+@app.route("/dictionary")
+def dictionaryHome():
+    text = "Choose a letter"
+    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    return render_template('dictionary.html', alphabet=alphabet, text=text)
+
+@app.route("/dictionary/<string:s>")
+def dictionary(s):
+    f = open('words.txt')
+    word_list = f.read().splitlines()
+    numberOfPrefix = 0
+    prefixPhrase = "with prefix"
+    for word in word_list:
+        if s.upper() == word[0:len(s)]:
+            numberOfPrefix += 1
+    is_real_word = s.upper() in word_list
+    alphabet = [s+"a",s+"b",s+"c",s+"d",s+"e",s+"f",s+"g",s+"h",s+"i",s+"j",s+"k",s+"l",s+"m",s+"n",s+"o",s+"p",s+"q",s+"r",s+"s",s+"t",s+"u",s+"v",s+"w",s+"x",s+"y",s+"z"]
+    return render_template('dictionary.html', s=s, is_real_word=is_real_word, alphabet=alphabet, numberOfPrefix=numberOfPrefix, prefixPhrase=prefixPhrase)
